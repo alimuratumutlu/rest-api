4c553884515a549fb7f97729713279f1e4a4a950
Murat Umutlu <muratumutlu@gmail.com>
2020-05-22 23:50:30 +0300

    Initial commit

---
+++ server.ts
@@ -0,0 +1,14 @@
+// Code to run localhost server.
+
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import router from "./routes.ts";
+const port = 8000;
+
+const app = new Application();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log(`Server running on port ${port}`);
+
+await app.listen({ port });
---
+++ types.ts
@@ -0,0 +1,8 @@
+// Contains model definition.
+
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+}
